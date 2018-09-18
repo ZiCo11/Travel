1e57b116743c734a6716e560736caef753a946cf
ZiCo11 <569653441@qq.com>
2018-09-18 21:12:12 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 解决移动端300毫秒延迟 引入fastclick
+import fastClick from 'fastclick'
+// 样式重置
+import './assets/styles/reset.css'
+// 1 像素边框解决方案
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+// 使用fastclick
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
