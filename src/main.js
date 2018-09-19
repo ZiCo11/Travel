f02fcf9b7c70631d82c710fd427e38c0cd13b70d
ZiCo11 <569653441@qq.com>
2018-09-19 16:08:42 +0800

    change

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入 swiper 插件 全局引用
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 // 解决移动端300毫秒延迟 引入fastclick
 import fastClick from 'fastclick'
 // 样式重置
@@ -11,10 +13,14 @@ import 'styles/reset.css'
 import 'styles/border.css'
 // 引入iconfont
 import 'styles/iconfont.css'
+// 引入swiper css文件
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 // 使用fastclick
 fastClick.attach(document.body)
+
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" ref="mySwiper" >
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swipe-img" :src="item.imgUrl" alt="去哪儿门票">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data: function () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/9b/8f79dbd1e0941302.jpg_750x200_55829659.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/cf/f9621afec6986e02.jpg_750x200_093f15c5.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: red !important
+  .wrapper
+    /* 这是图片宽高自适应样式 */
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    .swipe-img
+      width: 100%
+</style>
