e3bb45dfe14de761a682cbc328fffb1c5bec9866
ZiCo11 <569653441@qq.com>
2018-09-19 23:11:42 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="(item, index) of recommendList" :key="index">
+        <img class="item-img" :src="item.imgUrl">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-btn">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data: function () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1506/16/f05ab7bbcbdf7b7fdee652843ebba2db.water.jpg_200x200_abc1efd6.jpg',
+        title: '东平国家森林公园',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1802/d3/d345e53fdc97849ba3.img.jpg_200x200_c00f5662.jpg',
+        title: '东平国家森林公园',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1604/21/219fc64776b40b4490.img.jpg_200x200_bb9aa3a6.jpg',
+        title: '东平国家森林公园',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/mixins.styl"
+  .recommend-title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      .item-title
+        min-width: 0
+        line-height: .54rem
+        font-size: .32rem
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-btn
+        line-height: .44rem
+        background: #ff9300
+        padding: 0 .2rem
+        margin-top: .16rem
+        border-radius: .06rem
+</style>
