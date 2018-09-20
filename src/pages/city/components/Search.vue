a80038846257de2f178685212c5550838508fe5c
ZiCo11 <569653441@qq.com>
2018-09-20 15:41:27 +0800

    city search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input class="sear-input" type="text" placeholder="输入城市或拼音">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  .search
+    padding: 0 .1rem
+    height: .72rem
+    background: $bgColor
+    .sear-input
+      box-sizing: border-box
+      width: 100%
+      height: .62rem
+      padding: 0 .1rem
+      line-height: .62rem
+      text-align: center
+      border-radius: .1rem
+      color: #666
+</style>
